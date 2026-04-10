f42fd668eedf754eea1bf579edcf3a7d61cf4830
Moizkhan2903 <khanmoizmk03@gmail.com>
2026-04-10 19:25:21 +0530

    update

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -1,35 +1,46 @@
-18:50:13.500 
-   │ 
-18:50:13.500 
-   │ Help: 'src/app.routes.jsx' is imported by the following path:
-18:50:13.501 
-   │         - src/app.routes.jsx
-18:50:13.501 
-   │         - src/App.jsx
-18:50:13.501 
-   │         - src/main.jsx
-18:50:13.502 
-   │         - index.html
-18:50:13.502 
-───╯
-18:50:13.502 
-18:50:13.502 
-    at aggregateBindingErrorsIntoJsError (file:///vercel/path0/Frontend/node_modules/rolldown/dist/shared/error-CP8smW_P.mjs:48:18)
-18:50:13.503 
-    at unwrapBindingResult (file:///vercel/path0/Frontend/node_modules/rolldown/dist/shared/error-CP8smW_P.mjs:18:128)
-18:50:13.503 
-    at #build (file:///vercel/path0/Frontend/node_modules/rolldown/dist/shared/rolldown-build-4YnQkA76.mjs:3311:34)
-18:50:13.503 
-    at async buildEnvironment (file:///vercel/path0/Frontend/node_modules/vite/dist/node/chunks/node.js:32794:64)
-18:50:13.503 
-    at async Object.build (file:///vercel/path0/Frontend/node_modules/vite/dist/node/chunks/node.js:33216:19)
-18:50:13.503 
-    at async Object.buildApp (file:///vercel/path0/Frontend/node_modules/vite/dist/node/chunks/node.js:33213:153)
-18:50:13.504 
-    at async CAC.<anonymous> (file:///vercel/path0/Frontend/node_modules/vite/dist/node/cli.js:778:3) {
-18:50:13.504 
-  errors: [Getter/Setter]
-18:50:13.504 
-}
-18:50:13.521 
-Error: Command "npm run build" exited with 1
+import { createBrowserRouter } from "react-router";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+import Protected from "./features/auth/components/Protected";
+import Home from "./features/interview/pages/home";
+import Interview from "./features/interview/pages/Interview";
+import Privacy from "./features/Terms/Privacy";
+import TermsOfService from "./features/Terms/TermsOfService";
+import Help from "./features/Terms/Help";
+import Docs from "./features/Terms/Docs";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Protected><Home /></Protected>
+    },
+    {
+        path: "/login",
+        element: <Login />
+    },
+    {
+        path: "/register",
+        element: <Register />
+    },
+    {
+        path: "/interview/:interviewId",
+        element: <Protected><Interview /></Protected>
+    },
+    {
+        path: "/privacy",
+        element: <Privacy />
+    },
+    {
+        path: "/terms",
+        element: <TermsOfService />
+    },
+    {
+        path: "/help",
+        element: <Help />
+    },
+    {
+        path: "/docs",
+        element: <Docs />
+    }
+]);
+
